02288005ff0a73d5a5aebe5fe0cd8d423adb2f25
yya216 <yu_yuan_2@sfu.ca>
2023-05-30 10:31:03 -0700

    Update some page and index page

---
+++ index.js
@@ -0,0 +1,7 @@
+var github = document.getElementById('github');
+var linkedin = document.getElementById('linkedin');
+var wechat = document.getElementById('wechat');
+var instagram = document.getElementById('instagram');
+wechat.onclick = function(){
+
+}
---
+++ project.js
@@ -0,0 +1,9 @@
+var weatherapp = document.getElementById('weatherapp');
+var crazytranslator = document.getElementById('crazytranslator');
+
+weatherapp.onclick = function(){
+    URL('https://github.com/Jacksonyuan0326/Weather-APP');
+}
+crazytranslator.onclick = function(){
+    URL('https://frozenhydrant.github.io/found-in-translation/');
+}
