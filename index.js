4ee061d16bd41e08887c9ff1a0e6332411cd9bbd
yya216 <yu_yuan_2@sfu.ca>
2023-05-30 15:38:27 -0700

    Add software logo and add readme file

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+//navigation switch
+var education = document.querySelector('education-page');
+var project = document.querySelector('project-page');
+var experience = document.querySelector('experience-page');
+var contact = document.querySelector('contact-page');
+education.onclick = function(){
+    //it will redirect user to destiction
+}
+project.onclick = function(){
+    //it will redirect user to destiction
+}
+experience.onclick = function(){
+    //it will redirect user to destiction
+}
+    contact.onclick = function(){
+    //it will redirect user to destiction
+}
+
+//contact logo
 var github = document.getElementById('github');
 var linkedin = document.getElementById('linkedin');
 var wechat = document.getElementById('wechat');
 var instagram = document.getElementById('instagram');
-wechat.onclick = function(){
 
+github.onclick = function(){
+    //it will redirect user to my github
+}
+
+linkedin.onclick = function(){
+    //it will redirect user to my linkedin home page
+}
+
+wechat.onblur = function(){
+    //it will pop a small windows to show QR code
+}
+
+instagram.onblur = function(){
+    //it will pop a small windows to show QR code
 }
